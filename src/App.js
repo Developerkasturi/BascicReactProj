f5419cb6271f101cf3b4635e73158af65397cafc
Developerkasturi <120082472+Developerkasturi@users.noreply.github.com>
2024-07-12 13:03:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>React Project</h1>
+   <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+}
+
+export default App;
